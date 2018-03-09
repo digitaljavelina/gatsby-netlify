2faaacba91a2cd7457e6a5e14d8ea4aa16f41da2
Michael Henry <mike@digitaljavelina.com>
2018-03-09 15:27:30 -0700

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+	render() {
+		return (
+			<div>
+				<h1>About Me</h1>
+				<p>
+          Roof party pabst +1 chillwave, vexillologist selvage freegan tousled lyft food truck you probably haven't heard of them affogato succulents direct trade cronut. Irony austin chillwave bicycle rights lyft. Knausgaard chillwave live-edge pork belly mixtape artisan. 90's vexillologist messenger bag heirloom cray.
+          Microdosing iceland asymmetrical church-key enamel pin hashtag brunch pug. Quinoa farm-to-table pitchfork, brooklyn bespoke pork belly typewriter poutine 90's adaptogen schlitz asymmetrical banh mi you probably haven't heard of them plaid. Four loko mustache heirloom squid. Heirloom meh flexitarian green juice brunch hella narwhal chartreuse synth mixtape. Flannel pitchfork tousled microdosing kombucha, farm-to-table gluten-free food truck vexillologist next level pinterest jean shorts.
+
+        Shoreditch jean shorts squid pickled activated charcoal poke. Deep v tilde mixtape hell of. Vegan vice live-edge, tumblr crucifix squid small batch YOLO succulents jianbing pug bushwick hell of. Gluten-free tattooed quinoa microdosing cliche food truck schlitz beard.
+
+        Cold-pressed skateboard tbh helvetica irony vegan franzen photo booth vexillologist kale chips venmo meditation humblebrag pork belly crucifix. Artisan enamel pin four loko hashtag locavore, biodiesel VHS poutine tbh butcher activated charcoal vape glossier scenester gentrify. Mlkshk hammock pug aesthetic echo park listicle man braid iceland. Mustache knausgaard PBR&B, cliche keytar ramps bitters iPhone gochujang YOLO iceland williamsburg. Kickstarter offal intelligentsia butcher chia hoodie sriracha glossier adaptogen knausgaard woke copper mug tumeric forage paleo.
+
+        Stumptown meditation swag +1 bushwick. Truffaut forage trust fund, copper mug banjo prism lumbersexual four dollar toast health goth cloud bread hot chicken banh mi flexitarian lyft hoodie. Celiac roof party waistcoat fingerstache cardigan. Pinterest drinking vinegar copper mug bespoke glossier quinoa deep v. Celiac migas umami, mumblecore slow-carb iceland raclette hot chicken vaporware fixie hashtag aesthetic echo park. Venmo tumeric 8-bit, hammock gastropub marfa cloud bread franzen. Waistcoat authentic whatever cray chambray chillwave echo park offal pabst keytar kinfolk messenger bag blog.
+
+        Oh. You need a little dummy text for your mockup? How quaint.
+
+        I bet you’re still using Bootstrap too…
+				</p>
+			</div>
+		);
+	}
+}
---
+++ src/posts/our-second-post.md
@@ -0,0 +1,10 @@
+---
+title: "Our second post"
+date: "2018-03-09"
+---
+
+ug. Quinoa farm-to-table pitchfork, brooklyn bespoke pork belly typewriter poutine 90's adaptogen schlitz asymmetrical banh mi you probably haven't heard of them plaid. Four loko mustache heirloom squid. Heirloom meh flexitarian green juice brunch hella narwhal chartreuse synth mixtape. Flannel pitchfork tousled microdosing kombucha, farm-to-table gluten-free food truck vexillologist next level pinterest jean shIrony austin chillwave bicycle rights lyft. Knausgaard chillwave live-edge pork belly mixtape artisan. 90's vexillologist messenger bag heirloom cray. Microdosing iceland asymmetrical church-key enamel pin hashtag brunch pug. <!-- end --> farm-to-table pitchfork, brooklyn bespoke pork belly typewriter poutine 90's adaptogen schlitz asymmetrical banh mi you probably haven't heard of them plaid. Four loko mustache heirloom squid. Heirloom meh flexitarian green juice brunch hella narwhal chartreuse synth mixtape. Flannel pitchfork tousled microdosing kombucha, farm-to-table gluten-free food truck vexillologist next level pinterest jean shorts. Shoreditch jean shorts squid pickled activated charcoal poke. Deep v tilde mixtape hell of. Vegan vice llecore slow-carb iceland raclette hot chicken vaporware fixie hashtag aesthetic echo park. Venmo tumeric 8-bit, hammock gastropub marfa cloud bread franzen. Waistcoat authentic whatever cray chambray chillwave echo park offal pabst keytar kinfolk messenger bag blog. Oh. You need a little dummy text for your mockup? How quaint. I bet you’re still using Bootstrap too…
+
+
+
+
