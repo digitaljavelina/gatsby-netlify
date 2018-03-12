3e1efae09a989f0fc242e0e6caa8bbf5c1134e4e
Michael Henry <mike@digitaljavelina.com>
2018-03-11 22:24:01 -0700

    set up contentful api

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,40 +3,40 @@ module.exports = {
 		title: 'Gatsby Default Starter',
 		desc: 'A new blog',
 	},
-	plugins:
-		['gatsby-plugin-react-helmet',
-			'gatsby-plugin-styled-components',
-			{
-				resolve: 'gatsby-source-filesystem',
-				options: {
-					name: 'img',
-					path: `${__dirname}/src/images`
-				},
+	plugins:[
+		{
+			resolve: 'gatsby-source-contentful',
+			options: {
+				spaceId: 'hzzso42l9ejp',
+				accessToken: '7e05075be5e4e71e2fe8c2d20cf0eed4664fc370ac164284c6774030702be94f',
 			},
-			{
-				resolve: 'gatsby-source-filesystem',
-				options: {
-					name: 'src',
-					path: `${__dirname}/src/`
-				},
+		},
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-styled-components',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'img',
+				path: `${__dirname}/src/images`
 			},
-			{
-				resolve: 'gatsby-source-filesystem',
-				options: {
-					name: 'img',
-					path: `${__dirname}/static/assets`
-				},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'src',
+				path: `${__dirname}/src/`
 			},
-			{
-				resolve: 'gatsby-transformer-remark',
-				options: {
-					excerpt_separator: '<!-- end -->'
-				},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'img',
+				path: `${__dirname}/static/assets`
 			},
-			'gatsby-transformer-sharp',
-			'gatsby-plugin-sharp',
-			'gatsby-plugin-netlify-cms',
-		],
+		},
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+	],
 };
 
 // pathPrefix: '/levelup-blog',
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,24 +23,5 @@ export const query = graphql`
       desc
     }
   }
-  allMarkdownRemark(sort: {
-    fields: [frontmatter___date],
-    order: DESC
-  }) {
-    edges {
-      node {
-        id
-        frontmatter {
-          title
-          date(formatString: "MMMM DD, YYYY")
-        }
-        fields {
-          slug
-        }
-        html
-        excerpt(pruneLength: 280)
-      }
-    }
-  }
 }
 `;
